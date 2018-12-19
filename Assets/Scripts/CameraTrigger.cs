ad6c48752d9136c26ad35658a3343cf2617c6447
Andy Roberts <andrew.roberts86@hotmail.co.uk>
2018-12-19 17:47:51 +0000

    CamerasWorking

---
+++ Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour {
+    public Camera newCamera;
+    public Camera[] allCameras;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            for(int i = 0; i< allCameras.Length; i++)
+            {
+                allCameras[i].enabled = false;
+            }
+            newCamera.enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
