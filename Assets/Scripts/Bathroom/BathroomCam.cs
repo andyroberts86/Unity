1c05b1196bd4d40d570debffea84bad614864d6c
Andy Roberts EqualExperts <aroberts@equalexperts.com>
2018-12-27 20:24:00 +0000

    bathroom done

---
+++ Assets/Scripts/Bathroom/BathroomCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BathroomCam : MonoBehaviour
+{
+    public GameObject player;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(
+            player.transform.position.x,
+            player.transform.position.y + 10,
+            player.transform.position.z - 10);
+
+        cam.transform.position = newPos;
+    }
+}
---
+++ Assets/Scripts/Bathroom/Jump.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float height;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            other.GetComponent<Rigidbody>().AddForce(0f, height * 100, 0f);
+        }
+    }
+}
